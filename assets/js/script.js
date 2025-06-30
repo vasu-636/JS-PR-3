1118c6d9f9cdea0359001793761b38f4773b214e
vasu-636 <vasupatel632006@gmail.com>
2025-06-30 14:15:31 +0530

    some changes in Ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,59 @@
- $(document).ready(function () {
-      for (let i = 1; i <= 10; i++) {
-        $("#box-container").append(`<input type="number" id="box${i}" class="input-box">`);
+$(document).ready(function () {
+  let boxCount = parseInt(prompt("✨ How many input boxes do you want?", "10"));
+
+  if (isNaN(boxCount) || boxCount <= 0) {
+    alert("Invalid number! Defaulting to 10 boxes.");
+    boxCount = 10;
+  }
+
+  for (let i = 1; i <= boxCount; i++) {
+    $("#box-container").append(`<input type="number" id="box${i}" class="input-box">`);
+  }
+
+  $("#random").on("click", function () {
+    $(".input-box").removeClass("highlight");
+    for (let i = 1; i <= boxCount; i++) {
+      let randomValue = Math.floor(Math.random() * 100);
+      $(`#box${i}`).val(randomValue);
+    }
+  });
+
+  $("#clear").on("click", function () {
+    for (let i = 1; i <= boxCount; i++) {
+      $(`#box${i}`).val('');
+    }
+    $(".input-box").removeClass("highlight");
+  });
+
+  $("#max").on("click", function () {
+    let max = -Infinity;
+    let maxId = null;
+    $(".input-box").removeClass("highlight");
+    for (let i = 1; i <= boxCount; i++) {
+      let val = parseInt($(`#box${i}`).val());
+      if (val > max) {
+        max = val;
+        maxId = `#box${i}`;
       }
+    }
+    if (maxId) {
+      $(maxId).addClass("highlight");
+    }
+  });
 
-      $("#random").on("click", function () {
-        $(".input-box").removeClass("highlight");
-        for (let i = 1; i <= 10; i++) {
-          let randomValue = Math.floor(Math.random() * 100);
-          $(`#box${i}`).val(randomValue);
-        }
-      });
-
-      $("#clear").on("click", function () {
-        for (let i = 1; i <= 10; i++) {
-          $(`#box${i}`).val('');
-        }
-       
-      });
-
-      $("#max").on("click", function () {
-        let max = -Infinity;
-        let maxId = null;
-      $(".input-box").removeClass("highlight");
-        for (let i = 1; i <= 10; i++) {
-          let val = parseInt($(`#box${i}`).val());
-          if ( val > max) {
-            max = val;
-            maxId = `#box${i}`;
-          }
-        }
-        if (maxId) {
-          $(maxId).addClass("highlight");
-        }
-      });
-
-      $("#min").on("click", function () {
-        let min = Infinity;
-        let minId = null;
-       $(".input-box").removeClass("highlight");
-        for (let i = 1; i <= 10; i++) {
-          let val = parseInt($(`#box${i}`).val());
-          if (!isNaN(val) && val < min) {
-            min = val;
-            minId = `#box${i}`;
-          }
-        }
-
-        if (minId) {
-          $(minId).addClass("highlight");
-        }
-      });
-    });
+  $("#min").on("click", function () {
+    let min = Infinity;
+    let minId = null;
+    $(".input-box").removeClass("highlight");
+    for (let i = 1; i <= boxCount; i++) {
+      let val = parseInt($(`#box${i}`).val());
+      if (!isNaN(val) && val < min) {
+        min = val;
+        minId = `#box${i}`;
+      }
+    }
+    if (minId) {
+      $(minId).addClass("highlight");
+    }
+  });
+});
